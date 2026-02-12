fb7b066c50b250f29282c0374706b514c5c104a0
ps1312 <ps131296@gmail.com>
2026-02-12 09:16:00 -0300

    Initial Commit

---
+++ DiscogsClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiscogsClient
+//
+//  Created by paulo on 12/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiscogsClient/DiscogsClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscogsClientApp.swift
+//  DiscogsClient
+//
+//  Created by paulo on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DiscogsClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
